9932d810caf571b7a6065d937244fa8e41e0c90a
Anton Sokolskyi <sokolplus3783@gmail.com>
2023-05-19 00:01:46 +0300

    add save to files

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,19 @@
 package org.example;
 
 import java.util.Scanner;
+import org.example.menu.AbstractMenuCreator;
+import org.example.menu.AbstractMenuCreator.MainMenuCreator;
+import org.example.menu.Menu;
 
-public class App{
+public class App {
 
-    public final static Scanner IN = new Scanner(System.in);
+  public final static Scanner IN = new Scanner(System.in);
+  public static String path = "/data/";
 
-    public static void main( String[] args )    {
-        System.out.println( "Hello World!" );
-    }
+  public static void main(String[] args) {
+      Util.restoreData();
+      Menu menu = new MainMenuCreator().getMenu();
+      menu.runMenu();
+      Util.saveData();
+  }
 }
--- src/main/java/org/example/Util.java
+++ src/main/java/org/example/Util.java
@@ -1,5 +1,17 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import lombok.SneakyThrows;
+import org.example.dao.DAO;
+import org.example.dao.FabricatorDAO;
+import org.example.dao.SouvenirDAO;
 import org.example.models.Fabricator;
 import org.example.models.Souvenir;
 
@@ -16,5 +28,65 @@ public class Util {
 
   public static Souvenir enterSouvenir() {
     return null;
+  }
+
+
+  public static void restoreData() {
+    try {
+      restoreFabricator();
+      restoreSouvenirs();
+    } catch (FileNotFoundException e) {
+      return;
+    }
+  }
+
+  private static void restoreSouvenirs() throws FileNotFoundException {
+    Gson gson = new Gson();
+    List<Souvenir> coll = gson.fromJson(new FileReader(
+        App.path + Souvenir.class.getCanonicalName()
+    ), new TypeToken<List<Souvenir>>(){}.getType());
+    DAO<Souvenir> dao = SouvenirDAO.getInstance();
+    coll.forEach(dao::save);
+  }
+
+  private static void restoreFabricator() throws FileNotFoundException {
+    Gson gson = new Gson();
+    List<Fabricator> coll = gson.fromJson(new FileReader(
+        App.path + Fabricator.class.getCanonicalName()
+    ), new TypeToken<List<Fabricator>>(){}.getType());
+    DAO<Fabricator> dao = FabricatorDAO.getInstance();
+    coll.forEach(dao::save);
+  }
+
+  public static void saveData() {
+    try {
+      saveSouvenirs();
+    } catch (IOException e) {
+      System.out.println("Souvenris hasn't been saved");
+    }
+
+    try {
+      saveFabricator();
+    } catch (IOException e) {
+      System.out.println("Fabricators hasn't been saved");
+    }
+  }
+
+  private static void saveSouvenirs() throws IOException {
+    Gson gson = new Gson();
+    List all = SouvenirDAO.getInstance().getAll();
+    String serialized = gson.toJson(all);
+    try(FileWriter writer = new FileWriter(App.path + Souvenir.class.getCanonicalName())){
+      writer.write(serialized);
+    }
+  }
+
+  private static void saveFabricator() throws IOException {
+    Gson gson = new Gson();
+    List all = FabricatorDAO.getInstance().getAll();
+    String serialized = gson.toJson(all);
+    try(FileWriter writer = new FileWriter(App.path + Fabricator.class.getCanonicalName())){
+      writer.write(serialized);
+    }
   }
 }
